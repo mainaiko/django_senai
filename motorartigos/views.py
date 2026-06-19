6b163599619c188b9e9749a778dac679de7805be
natalia <mutantetrakor82@gmail.com>
2026-06-19 17:25:55 -0300

    hmtl(updates), views(update), footer(add _)

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 #import de um modulo hhtp do django de envio HTTP
 from django.http import HttpResponse
+from motorartigos.models import Autor
 
 # Create your views here.
 # regras de negocio
@@ -10,7 +11,25 @@ from django.http import HttpResponse
 #     return HttpResponse("<h1>Ola</h1>")
 
 def index(request):
-    return render(request, "motorartigos/index.html")
+    # estrutura de dados em python = collections dados guardados na memoria ram
+    # lista = [1,2,3,4,5,6]  - mutavel
+    # tupla = (1,2,3,4,5,6) - imutavel
+    #dicionario
+    #mock objects = dados de teste
+    """
+    autores ={
+        1:{"nome": "Natalia", "biografia": "a ++", "email": "natalia@gmail.com"},
+        2:{"nome": "luis", "biografia": "chosens", "email": "luis@gmail.com"},
+        3:{"nome": "Bruno", "biografia": "bruninhuuuu", "email": "bruno@gmail.com"},   
+    }
+    """
+    # objeto = instancia de uma classe (junçaõ de tudo que a classe é)
+    # classe = molde do objeto
+
+    autores = Autor.objects.all()
+
+    return render(request, "motorartigos/index.html", {"autores": autores})
 
 def artigo(request):
     return render(request, 'motorartigos/artigo.html')
+
